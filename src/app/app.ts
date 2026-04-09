af295ad45ef06f0800d00c73043c19208c7ba4b6
Ro <rostalbon@gmail.com>
2026-04-09 15:04:57 -0300

    Se importaron los componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './footer/footer';
+import { Header } from './header/header';
+import { Section } from './section/section';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Header, Section],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
