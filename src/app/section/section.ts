9883e0450856d058c93675f9c4f3dacfb7159129
Rocío Staltari <rostalbon@gmail.com>
2026-04-09 14:40:21 -0300

    Se cambió la configuración del index a español

---
+++ src/app/section/section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section',
+  imports: [],
+  templateUrl: './section.html',
+  styleUrl: './section.css',
+})
+export class Section {}
