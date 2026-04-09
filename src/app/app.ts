ff275221afaa70cb5dba19449b3a3679c34f3ce4
Ro <rostalbon@gmail.com>
2026-04-09 15:31:34 -0300

    Se modificó nav.html y se importaron los componentes faltantes

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { Footer } from './footer/footer';
 import { Header } from './header/header';
 import { Section } from './section/section';
+import { About } from './about/about';
+import { Projects } from './projects/projects';
+import { Contact } from './contact/contact';
+import { Nav } from './nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Header, Section],
+  imports: [RouterOutlet, Footer, Header, Section, Nav, About, Projects, Contact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
