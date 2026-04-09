c00c4c2f3d9b65e7550aa8a4f907948f6a0e7a2d
Ro <rostalbon@gmail.com>
2026-04-09 15:27:02 -0300

    Se añadió la etiqueta router-outlet a app.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Projects } from './projects/projects';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"about", component:About},
+    {path:"projects", component:Projects},
+    {path:"contact", component:Contact}
+];
