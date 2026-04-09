8c70e7c82f8f3141859eaa7e2581b0b4801faa0d
Ro <rostalbon@gmail.com>
2026-04-09 16:00:46 -0300

    Se modificó app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,14 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Footer } from './footer/footer';
 import { Header } from './header/header';
 import { Section } from './section/section';
-import { About } from './about/about';
-import { Projects } from './projects/projects';
-import { Contact } from './contact/contact';
 import { Nav } from './nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Header, Section, Nav, About, Projects, Contact],
+  imports: [RouterOutlet, Footer, Header, Section, Nav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
