52489bac2a9572349cd713b52d7871ad0130cb8d
Ro <rostalbon@gmail.com>
2026-04-23 14:19:41 -0300

    Se eliminó el componente main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { About } from './about/about';
 import { Projects } from './projects/projects';
 import { Contact } from './contact/contact';
-import { Main } from './main/main';
 
 export const routes: Routes = [
     {path:"about", component:About},
     {path:"projects", component:Projects},
     {path:"contact", component:Contact},
-    {path:"main", component:Main}
 ];
