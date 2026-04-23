9ee8c4d3136ea10ffffa68901cfeb61ffa985d37
Ro <rostalbon@gmail.com>
2026-04-23 14:15:39 -0300

    Se modificó app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { About } from './about/about';
 import { Projects } from './projects/projects';
 import { Contact } from './contact/contact';
+import { Main } from './main/main';
 
 export const routes: Routes = [
     {path:"about", component:About},
     {path:"projects", component:Projects},
-    {path:"contact", component:Contact}
+    {path:"contact", component:Contact},
+    {path:"main", component:Main}
 ];
