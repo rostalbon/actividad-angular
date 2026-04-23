2f6af87d92312ddef5bb3e1638dd94be7f928c98
Ro <rostalbon@gmail.com>
2026-04-23 14:23:22 -0300

    Se añadió el componente home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { About } from './about/about';
 import { Projects } from './projects/projects';
 import { Contact } from './contact/contact';
+import { Home } from './home/home';
 
 export const routes: Routes = [
     {path:"about", component:About},
     {path:"projects", component:Projects},
     {path:"contact", component:Contact},
+    {path:"home", component:Home}
 ];
